2296620c45b8acf0967ef6a997cc02760ef5b522
Leandro Martins <leandromartinspinto2004@gmail.com>
2024-09-12 13:16:47 -0300

    Criada a classe Reserva

---
+++ Amazing_Hotel/Entities/Reserva.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazing_Hotel.Entities
+{
+    class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+    }
+}
