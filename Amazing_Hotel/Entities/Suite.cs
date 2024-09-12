c5f0962b89c3e181bc567b3351f74f9c36ee0779
Leandro Martins <leandromartinspinto2004@gmail.com>
2024-09-12 13:11:30 -0300

    Criada a classe Suite

---
+++ Amazing_Hotel/Entities/Suite.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Amazing_Hotel.Entities
+{
+    class Suite
+    {
+        public string TipoSuite { get; set; }
+        public int Capacidade {  get; set; }
+        public decimal ValorDiaria { get; set; }
+    }
+}
